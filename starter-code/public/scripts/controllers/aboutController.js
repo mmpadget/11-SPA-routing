e699795e0d2d0bfd6a7d0b08bb8ca9fe44568df8
Michael Padget <mpadget@comcast.net>
2017-03-28 17:50:58 -0700

    Fixed article view and home page

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -7,13 +7,8 @@
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.init = function() {
     console.log('Inside of aboutController.init()');
-
-    // $('.main-nav').on('click', '.tab', function() {
-    // $('.tab-content').hide();
-    // $(`#${$(this).data('content')}`).fadeIn();
-    // });
-
-    // $('.main-nav .tab:first').click();
+    $('.tab-content').hide();
+    $('#about').show();
   };
 
   module.aboutController = aboutController;

c00274c8664677eaae20b9864e7b7b48640f388f
Michael Padget <mpadget@comcast.net>
2017-03-28 17:31:42 -0700

    Added routes, scripts to index page

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -2,9 +2,19 @@
 
 (function(module) {
   const aboutController = {};
+  console.log('About view initiated!');
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.init = function() {
+    console.log('Inside of aboutController.init()');
 
+    // $('.main-nav').on('click', '.tab', function() {
+    // $('.tab-content').hide();
+    // $(`#${$(this).data('content')}`).fadeIn();
+    // });
+
+    // $('.main-nav .tab:first').click();
+  };
 
   module.aboutController = aboutController;
 })(window);
--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -3,4 +3,10 @@
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
+page('/', articleController.init)
+page('/about', aboutController.init)
+// page('/new', newArticle.initNewArticlePage)
+// page('/admin', adminView.initAdminPage)
+
 // TODO: What function do you call to activate page.js? Fire it off now, to execute
+page();
